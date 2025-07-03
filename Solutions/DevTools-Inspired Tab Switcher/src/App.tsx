aaae46cdd760dadaeb792181e966789cd0ce906b
Arnav Kirti <arnav.tkd@gmail.com>
2025-07-04 03:00:21 +0530

    solutions started

---
+++ Solutions/DevTools-Inspired Tab Switcher/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+    <div className="flex justify-center items-center gap-5 p-4 border-b ">
+      <button className="border p-2">Elements</button>
+      <button className="border p-2">Console</button>
+      <button className="border p-2">Network</button>
+    </div>
+    </>
+  )
+}
+
+export default App
